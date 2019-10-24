07f1d6c46e5e3c3b29ae5d6ae49b03478d2e12dd
Logan Apple <loganapple@hotmail.com>
2019-10-23 21:52:57 -0700

    Added most functionality.

--- plot.py
+++ plot.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from PIL import Image, ImageDraw
+from matplotlib import pyplot as plt
 from colorhash import ColorHash
+from io import BytesIO
 
 import telegram
 from telegram.error import TelegramError
-
-import random
 
 with open("api_key.txt", 'r') as f:
     TOKEN = f.read().rstrip()
 
 bot = telegram.Bot(token=TOKEN)
 
-# I think it might be more elegant to return non-null and return strings with error text if need be.
+# I think it might be more elegant to return non-null and return strings with error text if need be. Sometimes,
+# however, I'll be returning non-errors, so I might want to implement a tuple system: (err_code, data)
+# Let 0 be success and 1 be some error.
 
 # https://www.science-emergence.com/Articles/How-to-put-the-origin-in-the-center-of-the-figure-with-matplotlib-/
 # https://pythonspot.com/matplotlib-scatterplot/
 # https://stackoverflow.com/questions/51113062/how-to-receive-images-from-telegram-bot
 
 class Plot:
-    def __init__(self, name, xaxisleft, xaxisright, yaxisbottom, yaxistop, minx, maxx, miny, maxy):
+    def __init__(self, name, xaxisleft, xaxisright, yaxisbottom, yaxistop, minx, maxx, miny, maxy, createdby):
         self.__name = name
         self.__xaxisleft = xaxisleft
         self.__xaxisright = xaxisright
@@ -32,12 +33,78 @@ class Plot:
         self.__miny = miny
         self.__maxy = maxy
         self.__points = []
+        self.__createdby = createdby
+
+    def __check_bounds(self, x, y):
+        if (self.__minx is not None and x < self.__minx) or (self.__maxx is not None and x > self.__maxx) or \
+                (self.__miny is not None and x < self.__miny) or (self.__maxy is not None and x > self.__maxy):
+            return False
+        return True
 
     def plot_point(self, label, x, y):
-        if not (self.__minx < x < self.__maxx and self.__miny < y < self.__maxy):
-            # Send error with bounds for plot name.
-            pass
-        self.__points.append((label, x, y))
+        if not self.__check_bounds(x, y):
+            return 1, "Error: Plot point cannot be out of bounds: " \
+                      "x : [" + str(self.__minx if self.__minx is not None else "_") + ", " + \
+                   str(self.__maxx if self.__maxx is not None else "_") + "] " + \
+                   "y : [" + str(self.__miny if self.__miny is not None else "_") + ", " + \
+                   str(self.__maxy if self.__maxy is not None else "_") + "]"
+
+        for i in range(len(self.__points)):
+            if self.__points[i][0] == label:
+                self.__points[i] = (label, x, y)
+                return 0, ""
+
+        self.__points.append((label if label is not None else "", x, y))
+
+        return 0, ""
+
+    def remove_point(self, label):
+        if label not in [t[0] for t in self.__points]:
+            return 1, "Error: You haven't plotted yourself in this plot."
+        self.__points.remove(next(p for p in self.__points if p[0] == label))
+
+        return 0, ""
+
+    def generate_plot(self):
+        X = [p[1] for p in self.__points]
+        Y = [p[2] for p in self.__points]
+        labels = [p[0] for p in self.__points]
+        colors = [(color_hash[0] / 255, color_hash[1] / 255, color_hash[2] / 255)
+                  for color_hash in [ColorHash(label).rgb for label in labels]]
+
+        fig = plt.figure()
+        plt.scatter(X, Y, c=colors)
+        plt.grid(True)
+        plt.axhline(y=0, color='k')
+        plt.axvline(x=0, color='k')
+
+        for i in range(len(X)):
+            plt.annotate(labels[i], (X[i], Y[i]))
+
+        if self.__xaxisleft is not None and self.__xaxisright is not None:
+            plt.xlabel("<-- " + str(self.__xaxisright) + " || " + str(self.__xaxisleft) + " -->")
+        elif self.__xaxisleft is not None:
+            plt.xlabel(str(self.__xaxisleft))
+        elif self.__xaxisright is not None:
+            plt.xlabel(str(self.__xaxisright))
+
+        if self.__yaxistop is not None and self.__yaxisbottom is not None:
+            plt.ylabel("<-- " + str(self.__yaxistop) + " || " + str(self.__yaxisbottom) + " -->")
+        elif self.__yaxistop is not None:
+            plt.xlabel(str(self.__yaxistop))
+        elif self.__yaxisbottom is not None:
+            plt.xlabel(str(self.__yaxisbottom))
+
+        if self.__name is not None:
+            plt.title(str(self.__name))
+
+        buffer = BytesIO()
+        fig.savefig(buffer, format="png")
+        buffer.seek(0)
+
+        # bot.send_photo(chat_id=chat_id, photo=buffer)
+        # This returns the image itself that can then be sent.
+        return 0, buffer
 
     def get_name(self):
         return self.__name
--- telegram_bot.py
+++ telegram_bot.py
@@ -7,6 +7,7 @@ from telegram.error import TelegramError
 import logging
 
 import os
+import argparse
 
 from plot import Plot
 
@@ -15,28 +16,51 @@ with open("api_key.txt", 'r') as f:
 
 PORT = int(os.environ.get('PORT', '8443'))
 
+ARG_PARSER = argparse.ArgumentParser(description="The parser for creating plots.")
+ARG_PARSER.add_argument("-t", "--title", type=str, nargs='*')
+ARG_PARSER.add_argument("-xr", "--xright", type=str, nargs='*')
+ARG_PARSER.add_argument("-xl", "--xleft", type=str, nargs='*')
+ARG_PARSER.add_argument("-yt", "--ytop", type=str, nargs='*')
+ARG_PARSER.add_argument("-yb", "--ybottom", type=str, nargs='*')
+ARG_PARSER.add_argument("-mx", "--minx", type=int)
+ARG_PARSER.add_argument("-Mx", "--maxx", type=int)
+ARG_PARSER.add_argument("-my", "--miny", type=int)
+ARG_PARSER.add_argument("-My", "--maxy", type=int)
 
-def createplot_handler(bot, update, chat_data, args):
+def send_message(bot, chat_id, text):
+    try:
+        # print(text)
+        bot.send_message(chat_id=chat_id, text=text)
+    except TelegramError as e:
+        raise e
+
+
+def static_handler(command):
+    text = open("static_responses/{}.txt".format(command), "r").read()
+
+    return CommandHandler(command,
+        lambda bot, update: send_message(bot, update.message.chat.id, text))
+
+
+def create_plot_handler(bot, update, chat_data, args):
     chat_id = update.message.chat.id
-    user_id = update.message.from_user.id
+    username = update.message.from_user.username
 
-    if len(args) <= 0:
-        # Need a name
-        pass
+    plot_args = vars(ARG_PARSER.parse_args(args))
 
-    if len(args) > 9:
-        # Can't do this!
-        pass
+    if len(plot_args.keys()) > 9:
+        send_message(bot, chat_id, "usage (all args optional): /createplot {title} {x_axis_right_label} "
+                                   "{x_axis_left_label} {y_axis_top_label} {y_axis_bottom_label}"
+                                   "{min_x_value} {max_x_value} {min_y_value} {max_y_value}")
+        return
 
     # I have two options as I see it:
     # (1) Use a counter as the key for the plots and iterate to find the name every time someone plots or removes.
     # (2) Use the name as the key and prevent duplicates. This means plots have to have a name.
+    # Now that I've thought about it, selecting by name is a terrible idea; titles could be extremely long. I'm
+    # going with option (1).
 
-    if args[0] in chat_data.keys():
-        # Can't have a duplicate name!
-        pass
-
-    # Args for plot definition (all are optional, except name in the current version):
+    # Args for plot definition (all are optional):
     # We'll have _ be a blank.
     # (1) name
     # (2) x-axis right title
@@ -48,36 +72,173 @@ def createplot_handler(bot, update, chat_data, args):
     # (8) min y value
     # (9) max y value
 
-    plot_args = [None for i in range(9)]
-    for i in range(len(args)):
-        plot_args[i] = None if args[i] == '_' else args[i]
+    plot = Plot(" ".join(plot_args.get("title", "")),
+                " ".join(plot_args.get("xright", "")),
+                " ".join(plot_args.get("xleft", "")),
+                " ".join(plot_args.get("ytop", "")),
+                " ".join(plot_args.get("ybottom", "")),
+                plot_args.get("minx"),
+                plot_args.get("maxx"),
+                plot_args.get("miny"),
+                plot_args.get("maxy"),
+                username)
+    chat_data[len(chat_data.keys()) + 1] = plot
 
-    plot = Plot(plot_args[0],
-                plot_args[1],
-                plot_args[2],
-                plot_args[3],
-                plot_args[4],
-                plot_args[5],
-                plot_args[6],
-                plot_args[7],
-                plot_args[8])
-    chat_data[args[0]] = plot
-
-    # Ideally, we'll be able to save the entire
-    # dictionary into a text file and load it
-    # from there.
+    send_message(bot, chat_id, str(" ".join(plot_args.get("title", ""))) + " (" + str(len(chat_data.keys())) + ") was created successfully!")
 
 
-def removeplot_handler(bot, update, chat_data, args):
+def remove_plot_handler(bot, update, chat_data, args):
     chat_id = update.message.chat.id
 
+    if len(args) != 1:
+        send_message(bot, chat_id, "usage: /removeplot {plot_id}")
+        return
 
-def plotme_handler(bot, update, chat_data, args):
+    try:
+        plot_id = int(args[0])
+    except TypeError:
+        send_message(bot, chat_id, "The plot ID must be a number!")
+        return
+
+    plot = chat_data.get(plot_id)
+
+    if plot is None:
+        send_message(bot, chat_id, "That plot doesn't exist!")
+        return
+
+    del chat_data[plot_id]
+
+
+def plot_me_handler(bot, update, chat_data, args):
+    chat_id = update.message.chat.id
+    username = update.message.from_user.username
+
+    # Args are: plot_id, x, y
+    if len(args) != 3:
+        send_message(bot, chat_id, "usage: /plotme {plot_id} {x} {y}")
+        return
+
+    try:
+        plot_id = int(args[0])
+        x = int(args[1])
+        y = int(args[2])
+    except TypeError:
+        send_message(bot, chat_id, "All input arguments must be integers!")
+        return
+
+    plot = chat_data.get(plot_id)
+
+    if plot is None:
+        send_message(bot, chat_id, "That plot doesn't exist!")
+        return
+
+    result = plot.plot_point(username, x, y)
+
+    if result is None:
+        return
+
+    if result[0] == 1:
+        send_message(bot, chat_id, result[1])
+        return
+    elif result[0] == 0:
+        img = plot.generate_plot()
+
+        if img is None:
+            return
+
+        if img[0] == 1:
+            send_message(bot, chat_id, img[1])
+            return
+        elif img[0] == 0:
+            bot.send_photo(chat_id=chat_id, photo=img[1])
+
+
+def remove_me_handler(bot, update, chat_data, args):
+    chat_id = update.message.chat.id
+    username = update.message.from_user.username
+
+    # Args are: plot_id
+    if len(args) != 1:
+        send_message(bot, chat_id, "usage: /removeme {plot_id}")
+        return
+
+    try:
+        plot_id = int(args[0])
+    except TypeError:
+        send_message(bot, chat_id, "The plot ID must be a number!")
+        return
+
+    plot = chat_data.get(plot_id)
+
+    if plot is None:
+        send_message(bot, chat_id, "That plot doesn't exist!")
+        return
+
+    result = plot.remove_point(username)
+
+    if result is None:
+        return
+
+    if result[0] == 1:
+        send_message(bot, chat_id, result[1])
+        return
+    elif result[0] == 0:
+        img = plot.generate_plot()
+
+        if img is None:
+            return
+
+        if img[0] == 1:
+            send_message(bot, chat_id, result[1])
+            return
+        elif img[0] == 0:
+            bot.send_photo(chat_id=chat_id, photo=result[1])
+
+
+def show_plot_handler(bot, update, chat_data, args):
     chat_id = update.message.chat.id
 
+    # Args are: plot_id
+    if len(args) != 1:
+        send_message(bot, chat_id, "usage: /showplot {plot_id}")
+        return
 
-def removeme_handler(bot, update, chat_data, args):
+    try:
+        plot_id = int(args[0])
+    except TypeError:
+        send_message(bot, chat_id, "The plot ID must be a number!")
+        return
+
+    plot = chat_data.get(plot_id)
+
+    if plot is None:
+        send_message(bot, chat_id, "That plot doesn't exist!")
+        return
+
+    result = plot.generate_plot()
+
+    if result is None:
+        return
+
+    if result[0] == 1:
+        send_message(bot, chat_id, result[1])
+        return
+    elif result[0] == 0:
+        bot.send_photo(chat_id=chat_id, photo=result[1])
+
+
+def list_plots_handler(bot, update, chat_data):
     chat_id = update.message.chat.id
+
+    text = "Current plots:\n\n"
+    for key in chat_data.keys():
+        text += "(" + str(key) + "): " + str(chat_data[key].get_name())
+    send_message(bot, chat_id, text)
+
+
+def edit_plot_handler(bot, update, chat_data, args):
+    chat_id = update.message.chat.id
+    username = update.message.from_user.username
 
 
 def handle_error(bot, update, error):
@@ -90,12 +251,25 @@ def handle_error(bot, update, error):
 if __name__ == "__main__":
     pp = PicklePersistence(filename="plotyourselfbot")
     bot = telegram.Bot(token=TOKEN)
-    updater = Updater(token=TOKEN, persistence=pp, use_context=True)
+    updater = Updater(token=TOKEN, persistence=pp)
     dispatcher = updater.dispatcher
 
+    static_commands = ["help"]
+    for c in static_commands:
+        dispatcher.add_handler(static_handler(c))
+
     create_plot_aliases = ["createplot", "cp"]
-    load_plots_aliases = ["loadplots", "lp"]
-    commands = [("createplot", 1, create_plot_aliases)]
+    plot_me_aliases = ["plotme", "pm", "plot"]
+    remove_me_aliases = ["removeme", "rm", "begone"]
+    remove_plot_aliases = ["removeplot", "rp"]
+    show_plot_aliases = ["showplot", "sp"]
+    list_plots_aliases = ["listplots", "lp"]
+    commands = [("create_plot", 2, create_plot_aliases),
+                ("plot_me", 2, plot_me_aliases),
+                ("remove_me", 2, remove_me_aliases),
+                ("remove_plot", 2, remove_plot_aliases),
+                ("show_plot", 2, show_plot_aliases),
+                ("list_plots", 1, list_plots_aliases)]
     for c in commands:
         func = locals()[c[0] + "_handler"]
         if c[1] == 0:
